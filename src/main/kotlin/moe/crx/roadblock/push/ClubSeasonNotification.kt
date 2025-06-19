804b874254fa0ec3a7239c3b21c63cb0df0438ea
Mathew Tkachuk <me@crx.moe>
2025-06-19 13:23:19 +0300

    feat: MultiplayerSeriesMaintenanceBookingNotification

---
+++ src/main/kotlin/moe/crx/roadblock/push/ClubSeasonNotification.kt
@@ -0,0 +1,22 @@
+package moe.crx.roadblock.push
+
+import moe.crx.roadblock.io.ObjectIO.readObject
+import moe.crx.roadblock.io.ObjectIO.writeObject
+import moe.crx.roadblock.io.sinks.InputSink
+import moe.crx.roadblock.io.sinks.OutputSink
+import moe.crx.roadblock.objects.game.StatusUpdatesQueueWithRootReactions
+
+class ClubSeasonNotification : PushMessagePacket() {
+
+    var updates: StatusUpdatesQueueWithRootReactions = StatusUpdatesQueueWithRootReactions()
+
+    override fun read(sink: InputSink) {
+        super.read(sink)
+        updates = sink.readObject()
+    }
+
+    override fun write(sink: OutputSink) {
+        super.write(sink)
+        sink.writeObject(updates)
+    }
+}
