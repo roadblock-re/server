391716929c8ce73fd52eafe0826494dc7aaa227d
Mathew Tkachuk <me@crx.moe>
2025-06-19 12:01:29 +0300

    feat: Push messages

---
+++ src/main/kotlin/moe/crx/roadblock/push/base/PushMessagePacket.kt
@@ -0,0 +1,27 @@
+package moe.crx.roadblock.push.base
+
+import moe.crx.roadblock.io.OptionalIO.readOptional
+import moe.crx.roadblock.io.OptionalIO.writeOptional
+import moe.crx.roadblock.io.sinks.InputSink
+import moe.crx.roadblock.io.sinks.OutputSink
+import moe.crx.roadblock.objects.game.Error
+import moe.crx.roadblock.rpc.base.Packet
+import moe.crx.roadblock.rpc.base.PacketDirection
+
+open class PushMessagePacket() : Packet(PacketDirection.SPECIAL) {
+
+    var type: Byte = 0
+    var error: Error? = null
+
+    override fun read(sink: InputSink) {
+        super.read(sink)
+        type = sink.readByte()
+        error = sink.readOptional()
+    }
+
+    override fun write(sink: OutputSink) {
+        super.write(sink)
+        sink.writeByte(type)
+        sink.writeOptional(error)
+    }
+}
