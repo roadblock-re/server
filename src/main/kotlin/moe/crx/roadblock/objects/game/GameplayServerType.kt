702f9d154cbafbf74295e75781b3867a6892877b
Mathew Tkachuk <me@crx.moe>
2025-06-19 12:59:52 +0300

    feat: MultiplayerSeriesMatchmakingFinished

--- src/main/kotlin/moe/crx/roadblock/objects/game/GameplayServerType.kt
+++ src/main/kotlin/moe/crx/roadblock/objects/game/GameplayServerType.kt
@@ -1,7 +1,9 @@
 package moe.crx.roadblock.objects.game
 
+// FYI: Original enum values names are in lower-case
 enum class GameplayServerType {
     Anubis,
     Embedded,
     Standalone,
+    Gamelift,
 }
