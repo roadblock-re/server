aed8c0ecbc421fa773273d311fff0c1f15a58352
Mathew Tkachuk <me@crx.moe>
2025-06-20 14:24:15 +0300

    refactor: certificate generation

--- src/main/kotlin/moe/crx/roadblock/SSLConfig.kt
+++ src/main/kotlin/moe/crx/roadblock/SSLConfig.kt
@@ -1,44 +1,107 @@
 package moe.crx.roadblock
 
-import io.ktor.network.tls.certificates.*
-import io.ktor.network.tls.extensions.*
 import io.ktor.server.engine.*
+import org.bouncycastle.asn1.x509.BasicConstraints
+import org.bouncycastle.asn1.x509.Extension
+import org.bouncycastle.asn1.x509.GeneralName
+import org.bouncycastle.asn1.x509.GeneralNames
+import org.bouncycastle.cert.X509v3CertificateBuilder
+import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter
+import org.bouncycastle.cert.jcajce.JcaX509v3CertificateBuilder
+import org.bouncycastle.jce.provider.BouncyCastleProvider
 import org.bouncycastle.openssl.jcajce.JcaPEMWriter
+import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder
 import java.io.File
 import java.io.FileInputStream
+import java.io.FileOutputStream
 import java.io.FileWriter
+import java.math.BigInteger
+import java.security.KeyPairGenerator
 import java.security.KeyStore
+import java.security.Security
+import java.security.cert.X509Certificate
+import java.util.*
 import javax.security.auth.x500.X500Principal
 
+fun generateKeyStore(
+    keyAlias: String,
+    keyStorePassword: String,
+    privateKeyPassword: String,
+    keyStoreFile: File,
+    pemFile: File,
+) {
+    val keyPairGen = KeyPairGenerator.getInstance("RSA")
+    keyPairGen.initialize(1024)
+    val keyPair = keyPairGen.generateKeyPair()
+
+    val now = Date()
+    val validUntil = Date(now.time + 3650L * 24 * 60 * 60 * 1000)
+    val serialNumber = BigInteger.valueOf(System.currentTimeMillis())
+
+    val subject = X500Principal("CN=eve.gameloft.com,OU=Unknown,O=Unknown,L=Unknown,ST=Unknown,C=Unknown")
+
+    val certBuilder: X509v3CertificateBuilder = JcaX509v3CertificateBuilder(
+        subject, serialNumber, now, validUntil, subject, keyPair.public
+    )
+
+    certBuilder.addExtension(
+        Extension.basicConstraints,
+        true,
+        BasicConstraints(true)
+    )
+
+    val san = GeneralNames(
+        arrayOf(
+            GeneralName(GeneralName.dNSName, "eve.gameloft.com")
+        )
+    )
+
+    certBuilder.addExtension(
+        Extension.subjectAlternativeName,
+        false,
+        san
+    )
+
+    val signer = JcaContentSignerBuilder("SHA384withRSA").build(keyPair.private)
+    val certificate: X509Certificate = JcaX509CertificateConverter()
+        .setProvider(BouncyCastleProvider())
+        .getCertificate(certBuilder.build(signer))
+
+    val keyStore = KeyStore.getInstance("JKS").apply {
+        load(null, null)
+        setKeyEntry(keyAlias, keyPair.private, privateKeyPassword.toCharArray(), arrayOf(certificate))
+    }
+
+    FileOutputStream(keyStoreFile).use {
+        keyStore.store(it, keyStorePassword.toCharArray())
+    }
+
+    JcaPEMWriter(FileWriter(pemFile)).use { pemWriter ->
+        pemWriter.writeObject(certificate)
+    }
+}
+
 fun ApplicationEngine.Configuration.sslConfig(sslPort: Int) {
-    val keyStoreFile = File("certs/keystore.jks")
-    val certFile = File("certs/certificate.cer")
+    Security.addProvider(BouncyCastleProvider())
 
     val keyAlias = "roadblock"
     val keyStorePassword = "password"
     val privateKeyPassword = "password"
+    val keyStoreFile = File("certs","roadblock.jks")
+    val pemFile = File("certs", "roadblock.cer")
 
     if (!keyStoreFile.exists()) {
-        val keyStore = buildKeyStore {
-            certificate(keyAlias) {
-                hash = HashAlgorithm.SHA384
-                sign = SignatureAlgorithm.RSA
-                password = privateKeyPassword
-                domains = listOf("eve.gameloft.com")
-                daysValid = 365 * 10
-                ipAddresses = listOf()
-                subject = X500Principal("CN=eve.gameloft.com,OU=Unknown,O=Unknown,L=Unknown,ST=Unknown,C=Unknown")
-            }
-        }
-        keyStore.saveToFile(keyStoreFile, keyStorePassword)
+        generateKeyStore(
+            keyAlias,
+            keyStorePassword,
+            privateKeyPassword,
+            keyStoreFile,
+            pemFile,
+        )
     }
 
     val keyStore = KeyStore.getInstance("JKS").apply {
         load(FileInputStream(keyStoreFile), keyStorePassword.toCharArray())
-    }
-
-    JcaPEMWriter(FileWriter(certFile)).use { certWriter ->
-        certWriter.writeObject(keyStore.run { getCertificate(aliases().nextElement()) })
     }
 
     sslConnector(
