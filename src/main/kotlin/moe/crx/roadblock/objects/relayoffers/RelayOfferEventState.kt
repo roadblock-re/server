37a5e8a025f8e3dcf417e018eddcf95698bd2f5b
Mathew Tkachuk <me@crx.moe>
2025-06-29 15:17:30 +0300

    refactor: Reworked RelayOfferEventState

--- src/main/kotlin/moe/crx/roadblock/objects/relayoffers/RelayOfferEventState.kt
+++ src/main/kotlin/moe/crx/roadblock/objects/relayoffers/RelayOfferEventState.kt
@@ -1,15 +1,26 @@
 package moe.crx.roadblock.objects.relayoffers
 
+import moe.crx.roadblock.io.MapIO.readMap
+import moe.crx.roadblock.io.MapIO.writeMap
 import moe.crx.roadblock.io.sinks.InputSink
 import moe.crx.roadblock.io.sinks.OutputSink
+import moe.crx.roadblock.objects.base.RByte
+import moe.crx.roadblock.objects.base.RInt
 import moe.crx.roadblock.objects.base.RObject
+import moe.crx.roadblock.objects.game.RelayOfferTierId
 
 class RelayOfferEventState : RObject {
+
+    var currentPurchasableTier: RelayOfferTierId = 0
+    var amountOfPurchasesPerTier: Map<RInt, RByte> = mapOf() // RelayOfferTierId
+
     override fun read(sink: InputSink) {
-        TODO("Not yet implemented")
+        currentPurchasableTier = sink.readInt()
+        amountOfPurchasesPerTier = sink.readMap()
     }
 
     override fun write(sink: OutputSink) {
-        TODO("Not yet implemented")
+        sink.writeInt(currentPurchasableTier)
+        sink.writeMap(amountOfPurchasesPerTier)
     }
 }
