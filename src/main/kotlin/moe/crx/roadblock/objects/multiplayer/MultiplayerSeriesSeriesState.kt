193a18ef396053687e2eb2900897b0684555c96b
Mathew Tkachuk <me@crx.moe>
2025-07-16 02:27:12 +0300

    feat: updated MultiplayerSeries for 24.6.0

--- src/main/kotlin/moe/crx/roadblock/objects/multiplayer/MultiplayerSeriesSeriesState.kt
+++ src/main/kotlin/moe/crx/roadblock/objects/multiplayer/MultiplayerSeriesSeriesState.kt
@@ -7,23 +7,31 @@ import moe.crx.roadblock.io.OptionalIO.writeOptional
 import moe.crx.roadblock.io.sinks.InputSink
 import moe.crx.roadblock.io.sinks.OutputSink
 import moe.crx.roadblock.objects.base.RFloat
+import moe.crx.roadblock.objects.base.RInstant
 import moe.crx.roadblock.objects.base.RObject
 import moe.crx.roadblock.objects.base.RString
 
 class MultiplayerSeriesSeriesState : RObject {
 
     var eloOfLastClosedEvent: RFloat? = null
+    var optLastClosedEventTimepoint: RInstant? = null
     var currentEventId: String = ""
     var events: Map<RString, MultiplayerSeriesEventState> = mapOf()
 
     override fun read(sink: InputSink) {
         eloOfLastClosedEvent = sink.readOptional()
+        if (sink newer "24.6.0") {
+            optLastClosedEventTimepoint = sink.readOptional()
+        }
         currentEventId = sink.readString()
         events = sink.readMap()
     }
 
     override fun write(sink: OutputSink) {
         sink.writeOptional(eloOfLastClosedEvent)
+        if (sink newer "24.6.0") {
+            sink.writeOptional(optLastClosedEventTimepoint)
+        }
         sink.writeString(currentEventId)
         sink.writeMap(events)
     }
