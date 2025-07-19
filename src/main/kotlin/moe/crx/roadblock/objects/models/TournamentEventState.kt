804fbd39a374f8a9f9ddfbfbc60afdc5728dc657
Mathew Tkachuk <me@crx.moe>
2025-07-20 02:05:21 +0300

    feat: TournamentSystem

---
+++ src/main/kotlin/moe/crx/roadblock/objects/models/TournamentEventState.kt
@@ -0,0 +1,15 @@
+package moe.crx.roadblock.objects.models
+
+import moe.crx.roadblock.io.sinks.InputSink
+import moe.crx.roadblock.io.sinks.OutputSink
+import moe.crx.roadblock.objects.base.RObject
+
+class TournamentEventState : RObject {
+
+    override fun read(sink: InputSink) {
+    }
+
+    override fun write(sink: OutputSink) {
+    }
+
+}
--- src/main/kotlin/moe/crx/roadblock/objects/models/TournamentSystemState.kt
+++ src/main/kotlin/moe/crx/roadblock/objects/models/TournamentSystemState.kt
@@ -1,17 +1,24 @@
 package moe.crx.roadblock.objects.models
 
+import moe.crx.roadblock.io.MapIO.readMap
+import moe.crx.roadblock.io.MapIO.writeMap
 import moe.crx.roadblock.io.sinks.InputSink
 import moe.crx.roadblock.io.sinks.OutputSink
 import moe.crx.roadblock.objects.base.RObject
+import moe.crx.roadblock.objects.base.RString
 
 class TournamentSystemState : RObject {
-    //highestPlacement
-    //eventDataById
+
+    var highestPlacement: Short = 0
+    var eventDataById: Map<RString, TournamentEventState> = mapOf()
+
     override fun read(sink: InputSink) {
-        TODO("Not yet implemented")
+        highestPlacement = sink.readShort()
+        eventDataById = sink.readMap()
     }
 
     override fun write(sink: OutputSink) {
-        TODO("Not yet implemented")
+        sink.writeShort(highestPlacement)
+        sink.writeMap(eventDataById)
     }
 }
