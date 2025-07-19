5ba70a912f0f37d69153541ba6b5c8583ffde91b
Mathew Tkachuk <me@crx.moe>
2025-07-20 01:28:43 +0300

    feat: VipSystem

--- src/main/kotlin/moe/crx/roadblock/objects/models/VipSystemState.kt
+++ src/main/kotlin/moe/crx/roadblock/objects/models/VipSystemState.kt
@@ -1,19 +1,34 @@
 package moe.crx.roadblock.objects.models
 
+import kotlinx.datetime.Clock.System.now
+import kotlinx.datetime.Instant
+import moe.crx.roadblock.io.ListIO.readList
+import moe.crx.roadblock.io.ListIO.writeList
+import moe.crx.roadblock.io.ObjectIO.readObject
+import moe.crx.roadblock.io.ObjectIO.writeObject
 import moe.crx.roadblock.io.sinks.InputSink
 import moe.crx.roadblock.io.sinks.OutputSink
+import moe.crx.roadblock.objects.base.RByte
 import moe.crx.roadblock.objects.base.RObject
 
 class VipSystemState : RObject {
-    //vipExperience
-    //vipBlackMarketState
-    //purchasedVIPBundleAtVIPLevel
-    //nextDailyGiftRestore
+
+    var vipExperience: Int = 0
+    var vipBlackMarketState: VipBlackMarketState = VipBlackMarketState()
+    var purchasedVIPBundleAtVIPLevel: List<RByte> = listOf()
+    var nextDailyGiftRestore: Instant = now()
+
     override fun read(sink: InputSink) {
-        TODO("Not yet implemented")
+        vipExperience = sink.readInt()
+        vipBlackMarketState = sink.readObject()
+        purchasedVIPBundleAtVIPLevel = sink.readList()
+        nextDailyGiftRestore = sink.readInstant()
     }
 
     override fun write(sink: OutputSink) {
-        TODO("Not yet implemented")
+        sink.writeInt(vipExperience)
+        sink.writeObject(vipBlackMarketState)
+        sink.writeList(purchasedVIPBundleAtVIPLevel)
+        sink.writeInstant(nextDailyGiftRestore)
     }
 }
