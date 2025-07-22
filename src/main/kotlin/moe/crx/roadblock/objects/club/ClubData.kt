79ba28484e503d3581bfaa0f436d6c238fa5de54
Mathew Tkachuk <me@crx.moe>
2025-07-22 23:11:10 +0300

    fix: CareerFinishRaceResponse

--- src/main/kotlin/moe/crx/roadblock/objects/club/ClubData.kt
+++ src/main/kotlin/moe/crx/roadblock/objects/club/ClubData.kt
@@ -6,11 +6,7 @@ import moe.crx.roadblock.io.OptionalIO.readOptional
 import moe.crx.roadblock.io.OptionalIO.writeOptional
 import moe.crx.roadblock.io.sinks.InputSink
 import moe.crx.roadblock.io.sinks.OutputSink
-import moe.crx.roadblock.objects.base.RBoolean
-import moe.crx.roadblock.objects.base.RInt
-import moe.crx.roadblock.objects.base.RObject
-import moe.crx.roadblock.objects.base.RShort
-import moe.crx.roadblock.objects.base.RString
+import moe.crx.roadblock.objects.base.*
 import moe.crx.roadblock.objects.game.ClubId
 import moe.crx.roadblock.objects.game.Credentials
 
--- src/main/kotlin/moe/crx/roadblock/rpc/response/CareerFinishRaceResponse.kt
+++ src/main/kotlin/moe/crx/roadblock/rpc/response/CareerFinishRaceResponse.kt
@@ -1,5 +1,30 @@
 package moe.crx.roadblock.rpc.response
 
+import moe.crx.roadblock.io.OptionalIO.readOptional
+import moe.crx.roadblock.io.OptionalIO.writeOptional
+import moe.crx.roadblock.io.sinks.InputSink
+import moe.crx.roadblock.io.sinks.OutputSink
+import moe.crx.roadblock.objects.base.RByte
 import moe.crx.roadblock.rpc.base.UpdatesQueueWithRootReactionsResponse
 
-class CareerFinishRaceResponse : UpdatesQueueWithRootReactionsResponse()
+class CareerFinishRaceResponse : UpdatesQueueWithRootReactionsResponse() {
+
+    var customAdsMultiplier: RByte? = null
+    var defaultAdsMultiplier: RByte? = null
+
+    override fun read(sink: InputSink) {
+        super.read(sink)
+        if (sink newer "45.0.0") {
+            customAdsMultiplier = sink.readOptional()
+            defaultAdsMultiplier = sink.readOptional()
+        }
+    }
+
+    override fun write(sink: OutputSink) {
+        super.write(sink)
+        if (sink newer "45.0.0") {
+            sink.writeOptional(customAdsMultiplier)
+            sink.writeOptional(defaultAdsMultiplier)
+        }
+    }
+}
