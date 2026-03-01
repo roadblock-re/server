1dafea6c2f197000e95be659118bf0538426b704
Mathew Tkachuk <me@crx.moe>
2026-03-01 22:12:21 +0300

    fix: CareerFinishRaceResponse

--- src/main/kotlin/moe/crx/roadblock/rpc/CareerFinishRace.kt
+++ src/main/kotlin/moe/crx/roadblock/rpc/CareerFinishRace.kt
@@ -5,10 +5,10 @@ import moe.crx.roadblock.game.GameConnection
 import moe.crx.roadblock.game.serialization.Blob
 import moe.crx.roadblock.game.serialization.ByteEnum
 import moe.crx.roadblock.game.serialization.FromVersion
-import moe.crx.roadblock.objects.transactions.CustomMultiplierType
 import moe.crx.roadblock.objects.RaceToken
 import moe.crx.roadblock.objects.multiplayer.RaceStats
 import moe.crx.roadblock.objects.quarantine.QuarantineData
+import moe.crx.roadblock.objects.transactions.CustomMultiplierType
 import moe.crx.roadblock.rpc.base.RequestPacket
 import moe.crx.roadblock.rpc.base.UpdatesQueueWithRootReactionsResponse
 
@@ -22,9 +22,9 @@ data class CareerFinishRaceRequest(
 
 @Serializable
 data class CareerFinishRaceResponse(
-    @FromVersion("45.0.0") @ByteEnum
+    @FromVersion("24.0.0") @ByteEnum
     var customAdsMultiplier: CustomMultiplierType? = null,
-    @FromVersion("45.0.0") @ByteEnum
+    @FromVersion("24.0.0") @ByteEnum
     var defaultAdsMultiplier: CustomMultiplierType? = null,
 ) : UpdatesQueueWithRootReactionsResponse()
 
