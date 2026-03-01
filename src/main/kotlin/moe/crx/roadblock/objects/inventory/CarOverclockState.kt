a5909c2a6407efea05de3746d30a55810c6e0a70
Mathew Tkachuk <me@crx.moe>
2026-03-02 00:03:29 +0300

    edit: disable car overclock

--- src/main/kotlin/moe/crx/roadblock/objects/inventory/CarOverclockState.kt
+++ src/main/kotlin/moe/crx/roadblock/objects/inventory/CarOverclockState.kt
@@ -1,11 +1,10 @@
 package moe.crx.roadblock.objects.inventory
 
-import kotlinx.datetime.Clock.System.now
 import kotlinx.datetime.Instant
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class CarOverclockState(
-    var overclockExpirationDateWithoutPass: Instant? = now(),
-    var overclockExpirationDate: Instant? = now(),
+    var overclockExpirationDateWithoutPass: Instant? = null,
+    var overclockExpirationDate: Instant? = null,
 )
