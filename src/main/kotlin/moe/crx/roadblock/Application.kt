671895bfb589b4cc087f43e4dce91544083ae864
Mathew Tkachuk <me@crx.moe>
2026-03-01 21:36:23 +0300

    edit: Add warning about disabled forced WebSocket

--- src/main/kotlin/moe/crx/roadblock/Application.kt
+++ src/main/kotlin/moe/crx/roadblock/Application.kt
@@ -1,12 +1,21 @@
 package moe.crx.roadblock
 
 import moe.crx.roadblock.core.*
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import java.io.File
+
+private val logger: Logger = LoggerFactory.getLogger("roadblock")
 
 object RoadblockServer {
     fun start(workingDirectory: String, wait: Boolean) {
         val config = readConfig(workingDirectory)
         config.writeConfig(workingDirectory)
+
+        if (!config.forceWebsocket) {
+            logger.warn("FORCED WEBSOCKET IS DISABLED. It is still not recommended because TCP implementation is rough.")
+        }
+
         httpServer(workingDirectory, config).start(wait = false)
         webSocketServer(workingDirectory, config).start(wait = false)
         tcpServer(workingDirectory, config, wait)
